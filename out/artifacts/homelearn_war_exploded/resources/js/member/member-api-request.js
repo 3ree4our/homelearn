14fcf5f27e6c41d6bbb2ed49ac2e6666cc0e762b
Hyobin Sung <128391669+hellmir@users.noreply.github.com>
2024-09-05 11:51:47 +0900

    refactor: 불필요한 디렉토리 삭제 (#67)
    - out/artifacts/homelearn_war_exploded

--- out/artifacts/homelearn_war_exploded/resources/js/member/member-api-request.js
+++
@@ -1,7 +0,0 @@
-import {SERVER_API} from "../common/request.js";
-
-const data = JSON.parse(localStorage.getItem('member'));
-export const getCoursesByMemberId = async (cp) => {
-  const result = await fetch(`${SERVER_API}/courses/${data.id}/${cp}/v1`);
-  console.log('강의 목록이 될 것' + result.json());
-}
